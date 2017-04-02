eb3fd35a6a22fb142b5c4030d38000a192b315d1
bapjiws <alex.boklin@gmail.com>
2017-04-02 16:04:32 +0300

    Implement a function to get the list of lesson links

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"strings"
 )
 
-var testUrl = "https://egghead.io/lessons/tools-share-a-tmux-session-for-pair-programming-with-ssh"
+//var lessonUrl = "https://egghead.io/lessons/tools-share-a-tmux-session-for-pair-programming-with-ssh"
+var courseUrl = "https://egghead.io/courses/wrangle-your-terminal-with-tmux"
 
 func getFileRef(n *html.Node) string {
 	nodeFound := false
@@ -46,8 +47,32 @@ func getFileRef(n *html.Node) string {
 	return strings.Replace(fileRef,".bin", "/file.mp4", 1)
 }
 
+func getLessons(n *html.Node) []string {
+	var lessons []string
+
+	var f func(n *html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode  && n.Data == "a"{
+			//fmt.Println(n)
+			for _, a := range n.Attr {
+				if a.Key == "href" && strings.Index(a.Val, "https://egghead.io/lessons/") != -1{
+					//fmt.Println(a)
+					lessons = append(lessons, a.Val)
+				}
+
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(n)
+
+	return lessons
+}
+
 func main() {
-	response, err := http.Get(testUrl)
+	response, err := http.Get(courseUrl)
 	if err != nil {
 		panic(err)
 	}
@@ -65,9 +90,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	lessons := getLessons(doc)
+	fmt.Println(lessons[0])
 
-	fileRef := getFileRef(doc)
-	fmt.Println(fileRef)
+	//fileRef := getFileRef(doc)
+	//fmt.Println(fileRef)
 	//res := fmt.Sprintf("https://embed-ssl.wistia.com/deliveries/%s/file.mp4", fileRef)
 	//fmt.Println(res)
 }
