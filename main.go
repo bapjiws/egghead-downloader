1e9333808ce2389e19db3c0b5e857c95ca6512aa
bapjiws <alex.boklin@gmail.com>
2017-04-02 16:14:24 +0300

    Get and parse html inside of getLessons and getFileRef

--- main.go
+++ main.go
@@ -11,9 +11,20 @@ import (
 //var lessonUrl = "https://egghead.io/lessons/tools-share-a-tmux-session-for-pair-programming-with-ssh"
 var courseUrl = "https://egghead.io/courses/wrangle-your-terminal-with-tmux"
 
-func getFileRef(n *html.Node) string {
-	nodeFound := false
+func getFileRef(url string) string {
+	var nodeFound bool
 	var fileRef string
+
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	doc, err := html.Parse(response.Body)
+	if err != nil {
+		panic(err)
+	}
 
 	var f func(n *html.Node)
 	f = func(n *html.Node) {
@@ -41,14 +52,25 @@ func getFileRef(n *html.Node) string {
 			f(c)
 		}
 	}
-	f(n)
+	f(doc)
 
 	// Substitute ".bin" with "/file.mp4"
 	return strings.Replace(fileRef,".bin", "/file.mp4", 1)
 }
 
-func getLessons(n *html.Node) []string {
+func getLessons(url string) []string {
 	var lessons []string
+
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	doc, err := html.Parse(response.Body)
+	if err != nil {
+		panic(err)
+	}
 
 	var f func(n *html.Node)
 	f = func(n *html.Node) {
@@ -66,18 +88,13 @@ func getLessons(n *html.Node) []string {
 			f(c)
 		}
 	}
-	f(n)
+	f(doc)
 
 	return lessons
 }
 
 func main() {
-	response, err := http.Get(courseUrl)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-
+	// This piece is useful to display the entire html.
 	//body, err := ioutil.ReadAll(response.Body)
 	//if err != nil {
 	//	panic(err)
@@ -86,15 +103,11 @@ func main() {
 	//res := string(body)
 	//fmt.Println(res)
 
-	doc, err := html.Parse(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	lessons := getLessons(doc)
+	lessons := getLessons(courseUrl)
 	fmt.Println(lessons[0])
 
-	//fileRef := getFileRef(doc)
-	//fmt.Println(fileRef)
+	fileRef := getFileRef(lessons[0])
+	fmt.Println(fileRef)
 	//res := fmt.Sprintf("https://embed-ssl.wistia.com/deliveries/%s/file.mp4", fileRef)
 	//fmt.Println(res)
 }
